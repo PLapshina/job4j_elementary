6567560f9cc06de2b9af7c48454f6b52c478b498
Polina Lapshina <110300326+PLapshina@users.noreply.github.com>
2023-02-08 23:46:25 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741] ИСПРАВЛЕНИЕ-2

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -11,8 +11,7 @@ public class Machine {
         for (int coin : coins) {
             while (change >= coin) {
                 change -= coin;
-                rsl[size] = coin;
-                size++;
+                rsl[size++] = coin;
             }
         }
         return Arrays.copyOf(rsl, size);
