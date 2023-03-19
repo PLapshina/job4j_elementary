07315896bcd7bd2d542f650f573a4dbac5479ae0
Polina Lapshina <110300326+PLapshina@users.noreply.github.com>
2023-03-19 17:26:47 +0300

    Чтение кода и базовые ошибки

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -16,7 +16,6 @@ public class Fit {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man is " + man);
-
         double woman = Fit.womanWeight(height);
         System.out.println("Woman is " + woman);
     }
--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -9,10 +9,8 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
-
         double result2 = Point.distance(3, 2, 2, 1);
         System.out.println("result (3, 2) to (2, 1) " + result2);
-
         double result3 = Point.distance(-1, 8, 4, -6);
         System.out.println("result (-1, 8) to (4, -6) " + result3);
     }
